9fe6cf6406daa7fe80f4e6a6b6155861c284c4c2
ozy-7 <72628688+ozy-7@users.noreply.github.com>
2023-08-13 18:03:02 +0300

    Update ElectricguitarsApplication.java

--- src/main/java/com/electricguitars/electricguitars/ElectricguitarsApplication.java
+++ src/main/java/com/electricguitars/electricguitars/ElectricguitarsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
 import java.util.Scanner;
 
 @SpringBootApplication
@@ -19,9 +20,55 @@ public class ElectricguitarsApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(ElectricGuitarDAO electricGuitarDAO) {
 		return runner -> {
-			createElectricGuitar(electricGuitarDAO);
+			//createElectricGuitar(electricGuitarDAO);
 			//findElectricGuitar(electricGuitarDAO);
+			//findAllElectricGuitars(electricGuitarDAO);
+			//findElectricGuitarByBrand(electricGuitarDAO);
+			changeElectricGuitar(electricGuitarDAO);
 		};
+	}
+
+	private void changeElectricGuitar(ElectricGuitarDAO electricGuitarDAO) {
+		Scanner scanner = new Scanner(System.in);
+
+		findAllElectricGuitars(electricGuitarDAO);
+
+		System.out.println("Enter the id of electric guitar to update: ");
+		int idToUpdate = scanner.nextInt();
+
+		ElectricGuitar theElectricGuitar = electricGuitarDAO.findById(idToUpdate);
+
+		theElectricGuitar.setBrand("PRS");
+		theElectricGuitar.setModel("Custom 24");
+		theElectricGuitar.setPickup("HH");
+		theElectricGuitar.setFretCount(24);
+		theElectricGuitar.setWoodType("Rosewood");
+
+		System.out.println("Updating electric guitar...");
+		electricGuitarDAO.update(theElectricGuitar);
+
+		System.out.println("Database updated. New electric guitars are: ");
+		findAllElectricGuitars(electricGuitarDAO);
+
+	}
+
+	private void findElectricGuitarByBrand(ElectricGuitarDAO electricGuitarDAO) {
+		String electricGuitarBrandToFind;
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter the brand of electric guitars that you want to retrieve: ");
+		electricGuitarBrandToFind = scanner.nextLine();
+
+		System.out.println(electricGuitarDAO.findByBrand(electricGuitarBrandToFind));
+	}
+
+	private void findAllElectricGuitars(ElectricGuitarDAO electricGuitarDAO) {
+		System.out.println("Getting all electric guitars...");
+		List<ElectricGuitar> allElectricGuitars = electricGuitarDAO.findAll();
+		for(ElectricGuitar tempElectricGuitar : allElectricGuitars) {
+			System.out.println(tempElectricGuitar);
+		}
+		return;
 	}
 
 	private void findElectricGuitar(ElectricGuitarDAO electricGuitarDAO) {
